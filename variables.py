858ef1e70f4f31c45d6db634e1c42a1a8d51d05e
imagekunwar <imagekunwar@gmail.com>
2026-02-24 01:01:03 -0600

    Day 2 Exercise 1 and 2

---
+++ variables.py
@@ -0,0 +1,24 @@
+#Day 2 Excersise 1
+first_name= "Image"
+last_name="Kunwar"
+full_name="Image Kunwar"
+country="Nepal"
+city="Kathmandu"
+age= 26
+year=2026
+is_married=False
+is_true= False
+is_light_on=False
+
+
+#Exercise 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
