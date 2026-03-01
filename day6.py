6634b1738e39b7766d18afb87099b0f5321b4498
imagekunwar <imagekunwar@gmail.com>
2026-02-28 23:56:25 -0600

    Day 6 Tuple lessons completed

--- day6.py
+++ day6.py
@@ -40,23 +40,56 @@ print('The count of siblings:',len(siblings))
 
 #5)Modify the siblings tuple and add the name of your father and mother and assign it to family_members
 tpl_to_list=list(siblings)
-tpl_to_list.append('Romio')
-tpl_to_list.append('Juliet')
+father="Romio"
+mother="Juliet"
+tpl_to_list.append(father)
+tpl_to_list.append(mother)
 print('Converting sibling tuples to list',tpl_to_list)
-list_to_tpl=tuple(tpl_to_list)
-print(list_to_tpl)
+family_member=tuple(tpl_to_list)
+print(family_member)
 
 
 #Exercises: Level 2
 
-#1)Unpack siblings and parents from family_members
+#1)Unpack siblings and parents from family_members  (look)
+siblings = family_member[:-2]
+parents = family_member[-2:]
+
+print("Siblings:", siblings)
+print("Parents:", parents)
+
 #2)Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
-#3)Change the about food_stuff_tp tuple to a food_stuff_lt list
-#4)Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
-#5)Slice out the first three items and the last three items from food_stuff_lt list
-#6)Delete the food_stuff_tp tuple completely
-#7)Check if an item exists in tuple:
-#8)Check if 'Estonia' is a nordic country
+fruits=('apple', 'grapes', 'mango', 'banana')
+vegetables=('cauliflower', 'onion', 'cabbage', 'radish')
+animal_products=('chicken','mutton','fish')
+food_stuff_tp= fruits + vegetables + animal_products
+print(food_stuff_tp)
+
+#3)Change the about food_stuff_tp tuple to a food_stuff_lt list.
+food_stuff_lt=list(food_stuff_tp)
+print(food_stuff_lt)
+
+#4)Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list. (look)
+middle_item_slicing=food_stuff_tp[-6:-5]
+print(middle_item_slicing)
+
+#5)Slice out the first three items and the last three items from food_stuff_lt list.
+# First 3 items
+first_three = food_stuff_tp[:3]
+
+# Last 3 items
+last_three = food_stuff_tp[-3:]
+
+print("First three items:", first_three)
+print("Last three items:", last_three)
+
+#6)Delete the food_stuff_tp tuple completely.
+del(food_stuff_tp)
+
+#8)Check if 'Estonia' is a nordic country.
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+checking='Estonia' in nordic_countries
+print(checking)
 
 """9)Check if 'Iceland' is a nordic country
 
